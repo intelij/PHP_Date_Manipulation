9fde6e76906940de3d07ed8661722ab46610915f
Khululekani <k.mkhonza@yahoo.co.uk>
2014-01-26 21:20:01 +0000

    Update meetingmanager.php

--- meetingmanager.php
+++ meetingmanager.php
@@ -1,14 +1,15 @@
 <?php
 
 /**
- * Short description for file
+ * 
+ * Below is the problem defination which this file is trying to address.
+ * 
  * The Client development team are required to have a mid month meeting in order to discuss the ongoing improvements 
  * and new features to the code base. This meeting is planned for the 14th of every month. Testing is also done on a 
  * monthly basis and should be done on the last day of the month.
  * If the 14th falls on a Saturday or Sunday then the mid month meeting should be arranged for the following Monday.
  * If the testing day falls on a Friday, Saturday or Sunday then testing should be set for the previous Thursday.
- * With this in mind, 
- * design a command line script that will write a CSV to file 
+ * With this in mind, design a command line script that will write a CSV to file 
  * outputting the columns 'Month', 'Mid Month Meeting Date' and 'End of Month Testing Date' for the next six months.
  *
  * PHP version 5 and greater
